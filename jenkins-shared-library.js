62c45852d12bfe19558551793bcd84847c521c43
M Hasan <hasanmuhammad197@gmail.com>
2025-10-26 09:17:00 +0700

    Shared library create

--- jenkins-shared-library.js
+++ jenkins-shared-library.js
@@ -5,3 +5,11 @@
 //Bisa import satu shared library ke beberapa job pipeline, ketika ada perubahan hanya perlu update shared library saja
 //Dengan membuat centralized repository yg berisikan pipeline, dan project yg membutuhkan pipeline tinggal menggunakan pipeline yg sudah dibuar shared library
 //Shared library membutuhkan SCM seperti Git, jadi pastikan nanti menyimpan Shared Library di project repo seperti Git
+
+//Create Shared Library
+//Untuk membuat shared library hanya perlu membuat folder berisi kode Groovy yg strukturnya sudah ditentukan, kemudian simpan di Git repository
+//Ini karena Jenkins Shared Library akan menggunakan Git untuk menyimpan & menggunakan di Jenkins Job yg membutuhkannya
+//Struktur folder shared library yaitu :
+//resource: berisi file yg bukan berisi kode program, misal text, json, gambar, dan lain lain
+//src: berisi class kode program Groovy
+//vars: berisi file groovy
